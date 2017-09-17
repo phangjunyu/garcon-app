258ecee6c1a2672e9b207ba19324606d277f5e5b
Xinchen <sinkozsinkoz@gmail.com>
2017-09-16 20:10:26 -0700

    added calling of endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  findMenu();
 });
+
+function findMenu(){
+  router.get('/menu/getmenu',function(req,res){
+    console.log(res);
+  })
+}
 
 module.exports = router;
