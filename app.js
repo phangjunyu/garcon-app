634af6c4c026e87d49c5eb505d10bdd5c859a0c8
phangjunyu <phangjunyu@gmail.com>
2017-09-16 20:15:24 -0700

    wdv

--- app.js
+++ app.js
@@ -25,6 +25,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/mainpage', function (req, res) {
+
+	var mainDish = {
+		title: 'Fish & Chips',
+		price: '$10',
+		description: 'tantalizing',
+		image: 'http://www.bizzielizzies.co.uk/wp-content/uploads/2015/03/slide-fish-chips-2015.jpg'
+	}
+	var otherDish = {
+		title: 'Ceasar Salad',
+		price: '$7',
+		description: 'not bad',
+		image: 'http://jetspizza.com/dbphotos/display/c161462910486f60cf38484ecf458adf/664/410'
+	}
+	var otherDishes = []
+	for(var i = 0; i < 10; i++){
+		otherDishes[i] = otherDish
+	}
+
+	var viewParameters = {
+		mainDish: mainDish,
+		otherDishes: otherDishes
+	}
+  res.render('mainpage', viewParameters)
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -42,5 +68,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000, function(){
+	console.log("app listening on port 3000!")
+})
 
 module.exports = app;
