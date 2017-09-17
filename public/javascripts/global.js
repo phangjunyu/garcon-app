e8103cbebb599d75ef6a5734da9daf913b0cf635
Xinchen <sinkozsinkoz@gmail.com>
2017-09-16 21:56:36 -0700

    added global js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,17 +1,18 @@
-// var http = require('http');
-// var client = http.createClient(8080,'localhost');
-//
-// $(document).ready(function() {
-//
-//     // Populate the user table on initial page load
-//     findMenu();
-//
-// });
-//
-//
-// function findMenu(){
-//     client.request(method='GET','/menu/getmenu',function(error,res){
-//         console.log(res.json(body));
-//     });
-//
-// }
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    findMenu();
+
+});
+
+
+function findMenu(){
+    $.getJSON('/menu/getmenu',function(data){
+        console.log("HIHIHI");
+        console.log(data);
+        $.each(data[0].menu,function(){
+            console.log(this.type);
+        })
+    })
+
+}
