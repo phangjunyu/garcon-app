676b60b76cec8ead87bfb594a3ea106093e88dba
Xinchen <sinkozsinkoz@gmail.com>
2017-09-16 22:22:32 -0700

    added global js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-
-    // Populate the user table on initial page load
-    findMenu();
-
-});
-
-
-function findMenu(){
-    $.getJSON('/menu/getmenu',function(data){
-        console.log("HIHIHI");
-        console.log(data);
-        $.each(data[0].menu,function(){
-            console.log(this.type);
-        })
-    })
-
-}
+// $(document).ready(function() {
+//
+//     // Populate the user table on initial page load
+//     findMenu();
+//
+// });
+//
+//
+// function findMenu(){
+//     $.getJSON('/menu/getmenu',function(data){
+//         console.log("HIHIHI");
+//         console.log(data);
+//         $.each(data[0].menu,function(){
+//             console.log(this.type);
+//         })
+//     })
+//
+// }
